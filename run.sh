ce620f33db4960f17ed4cd85d460d641d3ab5017
Youness Farini <yfarini@student.1337.ma>
2019-11-02 21:20:57 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 rm -rf EXPECTED OUT TESTS deepthought
 gcc -Wall -Wextra -Werror printf_main.c libftprintf.a
-valgrind --leak-check=full ./a.out > valgrind_out 2>&1
 ./a.out > OUT
 gcc -Wall -Wextra -Werror main.c gnl.a
 ./a.out
-rm -rf EXPECTED OUT TESTS
+rm -rf EXPECTED OUT TESTS a.out
